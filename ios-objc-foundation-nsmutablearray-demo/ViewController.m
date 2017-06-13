f0b97a617a18c1d681f97851cef8dbf55a363fed
e.kushida <e.kushida@st-ventures.jp>
2017-06-13 09:41:24 +0900

    Initial Commit

--- ios-objc-foundation-nsmutablearray-demo/ViewController.m
+++ ios-objc-foundation-nsmutablearray-demo/ViewController.m
@@ -16,14 +16,30 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    NSMutableArray<NSString*>* array = [@[@"月",@"火",@"木",@"金",@"土"] mutableCopy];
+
+    //1.1. 末尾に追加する
+    [array addObject:@"日"];
+
+    //1.2. 指定の場所にオブジェクトを追加する
+    [array insertObject:@"Wed" atIndex:2];
+
+    //1.3. 指定したオブジェクトを入れ替える
+    [array replaceObjectAtIndex:2 withObject: @"水"];
+
+    //1.4. 指定したindexのオブジェクトを削除する
+    [array removeObjectAtIndex:0];
+
+    //1.5. 指定のオブジェクトを削除する
+    [array removeObject:@"金"];
+
+    //1.6. すべてのオブジェクトを削除する
+    [array removeAllObjects];
+
+    for(NSString* a in array) {
+        NSLog(@"%@", a);
+    }
 }
-
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
 
 @end
